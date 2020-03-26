7ceae5cc0deda6b2f29fa99bdb04bbfe9d5fff58
Asia Szpadkiewicz <joanna.szpadkiewicz@gmail.com>
2020-03-26 15:49:19 +0100

    .env file was added

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -1,19 +1,11 @@
 import app from 'firebase/app';
-
-const config = {
-    apiKey: "AIzaSyAWcfbpQXUNDm5dkn4IjGnkUifr49LaxvE",
-    authDomain: "fav-market.firebaseapp.com",
-    databaseURL: "https://fav-market.firebaseio.com",
-    projectId: "fav-market",
-    storageBucket: "fav-market.appspot.com",
-    messagingSenderId: "902225836330",
-    appId: "1:902225836330:web:2d9a32756e639a25fc4653",
-    measurementId: "G-P7F7SZ7TPR"
-};
+import 'firebase/auth';
 
 class Firebase {
     constructor() {
         app.initializeApp(config);
+
+        this.db = app.database();
     }
 }
 export default Firebase;
