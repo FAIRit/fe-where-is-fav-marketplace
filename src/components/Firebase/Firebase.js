05511237f9d0b9e36ff63f941c820aa663120ee8
Asia Szpadkiewicz <joanna.szpadkiewicz@gmail.com>
2020-03-31 14:36:21 +0200

    firebase is starting

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -1,11 +1,40 @@
-import app from 'firebase/app';
-import 'firebase/auth';
+import React from "react";
+import firebase from "firebase";
+import config from './config';
 
-class Firebase {
-    constructor() {
-        app.initializeApp(config);
+class Firebase extends React.Component {
+    constructor(props) {
+        super(props);
+        firebase.initializeApp(config);
 
-        this.db = app.database();
+        this.state = {
+            markets: []
+        }
     }
+
+    writeUserData = () => {
+        firebase.database().ref('/').set(this.state);
+        console.log('DATA SAVED');
+    };
+
+       getUserData = () => {
+        let ref = Firebase.database().ref('/');
+        ref.on('value', snapshot => {
+            const state = snapshot.val();
+            this.setState(state);
+        });
+        console.log('DATA RETRIEVED');
+    };
+
+    componentDidMount() {
+        this.getUserData();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState !== this.state) {
+            this.writeUserData();
+        }
+    }
+
 }
 export default Firebase;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Firebase, {FirebaseContext} from './components/Firebase';
 
 import * as serviceWorker from './serviceWorker';
 import './../node_modules/leaflet/dist/leaflet.css'
 
 
 ReactDOM.render(
-    <FirebaseContext.Provider value={new Firebase()}>
-        <App/>
-    </FirebaseContext.Provider>,
+        <App/>,
     document.getElementById('root'));
 
 
